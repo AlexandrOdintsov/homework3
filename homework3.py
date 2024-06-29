899e50f91b570a01ff2f02640778067616dec90a
AlexandrOdintsov <alexodin83@mail.ru>
2024-06-29 19:07:49 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,9 @@
+name="Alexandr"
+print ("Name: ", name,type(name))
+age=40
+print ("Age: ",age, type(age))
+newage=age+15
+print ("Newage: ",newage, type(newage))
+is_student = True
+print ("Is_student: ", is_student)
+
